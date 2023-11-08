11bc5e2f29da526e8a83fb1a8b697166cef5095c
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2023-11-08 12:01:11 +0530

    changed name of files

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const person = require("./second")
-console.log(person)
-console.log(person.age)
-console.log(exports, require, module, __filename, __dirname)
+import http, { createServer, } from 'http';
+import { Console } from 'console';
+import fs from 'fs';
+
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html')
+    if (req.url == '/') {
+        let a = fs.readFileSync('index.html')
+        console.log(a.toString);
+        res.end(a);
+
+    }
+    else if (req.url == '/about') {
+        res.end(`<h1>This is Yogesh About</h1>`)
+    }
+    else if (req.url == '/downloads') {
+        res.end(`<h1>This is the download section where you can download the question papers</h1>`)
+    }
+    else {
+        res.statusCode = 404
+        res.end(`${res.statusCode}<h1>Page not found</h1>`)
+    }
+})
+server.listen(port, () => {
+    console.log(`Server listening of port ${port}`)
+})
---
+++ index2.js
@@ -0,0 +1,4 @@
+const person = require("./second")
+console.log(person)
+console.log(person.age)
+console.log(exports, require, module, __filename, __dirname)
